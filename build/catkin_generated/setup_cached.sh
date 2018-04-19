0904a53b075439968baa2b2b4039a0947c15d0dc
Neil Nie <yongyang.nie@gmail.com>
2018-04-19 12:53:49 -0400

    Learning ROS services, implementing arm mover

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/opt/ros/kinetic/lib"
-export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/yongyang/Developer/LearnROS/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/yongyang/Developer/LearnROS/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/yongyang/Developer/LearnROS/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/yongyang/Developer/LearnROS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yongyang/Developer/LearnROS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/yongyang/Developer/LearnROS/src:$ROS_PACKAGE_PATH"
